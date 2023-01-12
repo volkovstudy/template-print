6797f5b6a1e7d43a760e13241cea9a58fc52beab
Volkov Daniil <volkov.study@mail.ru>
2023-01-12 20:28:02 +0300

    Add test with integer container in TestRunner.cpp

--- tests/TestRunner.cpp
+++ tests/TestRunner.cpp
@@ -1,1 +1,25 @@
+#include <vector>
+#include <sstream>
 #include "TestRunner.h"
+
+void testContainerWithIntegersAndCommaDelimiter();
+
+void testContainerWithIntegersAndCommaDelimiter() {
+    // Given
+    vector<int> container = {1, 2, 3, 4};
+    string delimiter = ", ";
+
+    std::ostringstream oss;
+    std::streambuf* coutStreambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+
+    // When
+    Print(container, delimiter);
+
+    std::cout.rdbuf(coutStreambuf);
+
+    // Check
+    assert(oss.str() == "1, 2, 3, 4\n");
+
+    cout << "Test with integers passed" << endl;
+}
