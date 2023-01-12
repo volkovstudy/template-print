9206c4ef62a8f25c248200e37b0ecca0ed3d07dc
Volkov Daniil <volkov.study@mail.ru>
2023-01-12 20:33:05 +0300

    Add test with container of strings

--- tests/TestRunner.cpp
+++ tests/TestRunner.cpp
@@ -3,9 +3,11 @@
 #include "TestRunner.h"
 
 void testContainerWithIntegersAndCommaDelimiter();
+void testContainerWithStringsAndCommaDelimiter();
 
 void TestRunner::runAllTests() {
     testContainerWithIntegersAndCommaDelimiter();
+    testContainerWithStringsAndCommaDelimiter();
 }
 
 void testContainerWithIntegersAndCommaDelimiter() {
@@ -26,4 +28,24 @@ void testContainerWithIntegersAndCommaDelimiter() {
     assert(oss.str() == "1, 2, 3, 4\n");
 
     cout << "Test with integers passed" << endl;
+}
+
+void testContainerWithStringsAndCommaDelimiter() {
+    // Given
+    vector<string> container = {"a", "b", "c", "d"};
+    string delimiter = ", ";
+
+    std::ostringstream oss;
+    std::streambuf* coutStreambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+
+    // When
+    Print(container, delimiter);
+
+    std::cout.rdbuf(coutStreambuf);
+
+    // Check
+    assert(oss.str() == "a, b, c, d\n");
+
+    cout << "Test with strings passed" << endl;
 }
